a6da3b8168b092dc91c8527b1a8722101c217712
Björn Hedblom <git@singali.net>
2022-12-16 17:55:39 +0100

    day 1

--- src/day01.test.ts
+++ src/day01.test.ts
@@ -1,14 +1,16 @@
 import { day01 } from "./day01"
-import {describe, test, expect } from "@jest/globals"
+import { describe, test, expect } from "@jest/globals"
 
 describe("Day01", () => {
     test("part 1", () => {
         expect(day01.part1(input)).toEqual("24000")
     })
+    test("part 2", () => {
+        expect(day01.part2(input)).toEqual("45000")
+    })
 })
 
-const input =
-`1000
+const input = `1000
 2000
 3000
 
--- src/day01.ts
+++ src/day01.ts
@@ -1,8 +1,17 @@
 export const day01 = {
-    part1: (input:string):string => {
-        return ""
+    part1: (input: string): string => Math.max(...getElves(input)).toString(),
+    part2: (input: string): string => {
+        const elves = getElves(input)
+        elves.sort((a, b) => a - b)
+        return elves.slice(-3).reduce(sum).toString()
     },
-    part2: (input:string):string => {
-        return ""
-    }
+}
+const sum = (prev: number, current: number): number => prev + current
+function getElves(input: string) {
+    return input.split("\n\n").map(elfString =>
+        elfString
+            .split("\n")
+            .map(s => +s)
+            .reduce(sum),
+    )
 }
