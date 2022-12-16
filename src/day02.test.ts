506e7185a6fee4343bbcd11d1c91d442d9d8d8fe
Björn Hedblom <git@singali.net>
2022-12-16 19:04:40 +0100

    day 2

--- src/day02.test.ts
+++ src/day02.test.ts
@@ -1,13 +1,15 @@
-import {describe, test, expect } from "@jest/globals"
+import { describe, test, expect } from "@jest/globals"
 import { day02 } from "./day02"
 
 describe("Day02", () => {
     test("part 1", () => {
         expect(day02.part1(input)).toEqual("15")
     })
+    test("part 2", () => {
+        expect(day02.part2(input)).toEqual("12")
+    })
 })
 
-const input =
-`A Y
+const input = `A Y
 B X
 C Z`
--- src/day02.ts
+++ src/day02.ts
@@ -1,8 +1,107 @@
-export const day02 = {
-    part1: (input:string):string => {
-        return ""
-    },
-    part2: (input:string):string => {
-        return ""
+const [loss, draw, win] = [0, 3, 6]
+
+type MoveName = "rock" | "paper" | "scissors"
+
+interface Move {
+    name: MoveName
+    score: number
+    vs: {
+        rock: number
+        paper: number
+        scissors: number
     }
+}
+
+const rock: Move = {
+    name: "rock",
+    score: 1,
+    vs: {
+        rock: draw,
+        paper: loss,
+        scissors: win,
+    },
+}
+
+const paper: Move = {
+    name: "paper",
+    score: 2,
+    vs: {
+        rock: win,
+        paper: draw,
+        scissors: loss,
+    },
+}
+
+const scissors: Move = {
+    name: "scissors",
+    score: 3,
+    vs: {
+        rock: loss,
+        paper: win,
+        scissors: draw,
+    },
+}
+
+const moves = [rock, paper, scissors]
+
+const asOutcome = (str: string) => {
+    switch (str) {
+        case "X":
+            return loss
+
+        case "Y":
+            return draw
+
+        case "Z":
+            return win
+
+        default:
+            throw Error(`Unkown outcome: ${str}`)
+    }
+}
+
+const asMove = (movestring: string): Move => {
+    switch (movestring) {
+        case "A":
+        case "X":
+            return rock
+
+        case "B":
+        case "Y":
+            return paper
+
+        case "C":
+        case "Z":
+            return scissors
+
+        default:
+            throw Error(`unkown move: ${movestring}`)
+    }
+}
+
+const generalSolver = (
+    input: string,
+    roundsolver: (round: string) => number,
+): string =>
+    input
+        .split("\n")
+        .filter(x => !!x)
+        .map(roundsolver)
+        .reduce((a, b) => a + b)
+        .toString()
+
+export const day02 = {
+    part1: (input: string): string =>
+        generalSolver(input, round => {
+            const [opMove, myMove] = round.split(" ").map(asMove)
+            return myMove.score + myMove.vs[opMove.name]
+        }),
+    part2: (input: string): string =>
+        generalSolver(input, round => {
+            const [opStr, outcomeStr] = round.split(" ")
+            const opMove = asMove(opStr)
+            const outcome = asOutcome(outcomeStr)
+            const myMove = moves.filter(m => m.vs[opMove.name] === outcome)[0]
+            return myMove.score + myMove.vs[opMove.name]
+        }),
 }
