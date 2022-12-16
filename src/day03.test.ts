b9ee429285a1a3b2a5d903da6369d1191737a8df
Björn Hedblom <git@singali.net>
2022-12-16 23:16:47 +0100

    day 3

--- src/day03.test.ts
+++ src/day03.test.ts
@@ -1,14 +1,16 @@
 import { day03 } from "./day03"
-import {describe, test, expect } from "@jest/globals"
+import { describe, test, expect } from "@jest/globals"
 
 describe("Day01", () => {
     test("part 1", () => {
         expect(day03.part1(input)).toEqual("157")
     })
+    test("part 2", () => {
+        expect(day03.part2(input)).toEqual("70")
+    })
 })
 
-const input =
-`vJrwpWtwJgWrhcsFMMfFFhFp
+const input = `vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
--- src/day03.ts
+++ src/day03.ts
@@ -1,8 +1,48 @@
+import { sum } from "./utils"
+
+const CHARCODE_a = 97
+const OFFSET_a_to_1 = CHARCODE_a - 1
+const CHARCODE_A = 65
+const OFFSET_A_to_27 = CHARCODE_A - 27
+
+const itemPriority = (c: string | undefined) => {
+    if (!c) throw Error("This won't work!")
+    const charcode = c.charCodeAt(0)
+    return charcode >= CHARCODE_a
+        ? charcode - OFFSET_a_to_1
+        : charcode - OFFSET_A_to_27
+}
+
 export const day03 = {
-    part1: (input:string):string => {
-        return ""
+    part1: (input: string): string =>
+        input
+            .split("\n")
+            .flatMap(rucksack => {
+                const items = rucksack.split("")
+                const right = new Set(items.splice(items.length / 2))
+                const inBoth = new Set(items.filter(item => right.has(item)))
+                const left = new Set(items)
+                right.forEach(item => {
+                    if (left.has(item) && !inBoth.has(item)) inBoth.add(item)
+                })
+                return [...inBoth]
+            })
+            .map(itemPriority)
+            .reduce(sum)
+            .toString(),
+    part2: (input: string): string => {
+        const rucksacks = input.split("\n").filter(r => !!r)
+        let priority = 0
+        while (rucksacks.length) {
+            const group = rucksacks.splice(0, 3)
+            const first = new Set(group[0].split(""))
+            const second = new Set(group[1].split(""))
+            const badge = group[2]
+                .split("")
+                .filter(c => first.has(c))
+                .find(c => second.has(c))
+            priority += itemPriority(badge)
+        }
+        return priority.toString()
     },
-    part2: (input:string):string => {
-        return ""
-    }
 }
