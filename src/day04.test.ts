1f50b884f080c394929a7f04a018db6ce9c8538d
Björn Hedblom <git@singali.net>
2022-12-18 16:13:43 +0100

    day 4

--- src/day04.test.ts
+++ src/day04.test.ts
@@ -1,14 +1,16 @@
 import { day04 } from "./day04"
-import {describe, test, expect } from "@jest/globals"
+import { describe, test, expect } from "@jest/globals"
 
 describe("Day01", () => {
     test("part 1", () => {
         expect(day04.part1(input)).toEqual("2")
     })
+    test("part 2", () => {
+        expect(day04.part2(input)).toEqual("4")
+    })
 })
 
-const input =
-`2-4,6-8
+const input = `2-4,6-8
 2-3,4-5
 5-7,7-9
 2-8,3-7
--- src/day04.ts
+++ src/day04.ts
@@ -1,8 +1,42 @@
-export const day04 = {
-    part1: (input:string):string => {
-        return ""
-    },
-    part2: (input:string):string => {
-        return ""
+import { isMainThread } from "worker_threads"
+
+class Range {
+    readonly start: number
+    readonly end: number
+    constructor(dashSeparated: string) {
+        const [start, end] = dashSeparated.split("-")
+        this.start = +start
+        this.end = +end
     }
+
+    contains(other: Range) {
+        return this.start <= other.start && this.end >= other.end
+    }
+
+    overlaps(other: Range) {
+        if (this.end < other.start || this.start > other.end) return false
+        return true
+    }
+}
+
+const generalSolver = (
+    input: string,
+    checkOverlap: (first: Range, second: Range) => boolean,
+): string => {
+    let overlaps = 0
+    input.split("\n").forEach(pairs => {
+        const [first, second] = pairs.split(",").map(r => new Range(r))
+        if (checkOverlap(first, second)) overlaps += 1
+    })
+    return overlaps.toString()
+}
+
+export const day04 = {
+    part1: (input: string): string =>
+        generalSolver(
+            input,
+            (first, second) => first.contains(second) || second.contains(first),
+        ),
+    part2: (input: string): string =>
+        generalSolver(input, (first, second) => first.overlaps(second)),
 }
